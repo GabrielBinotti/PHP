87cd83f8fe03d37d3234e10c573c707e1d74bbfa
Gabriel Binotti <gabrielbinotti.dev@gmail.com>
2024-10-26 17:01:12 -0500

    Contact Form

---
+++ LoadingInternalPage/public/contato.php
@@ -0,0 +1,43 @@
+<h1 class="title">Contato</h1>
+<form action="../server/contato.php" method="post">
+    <div>
+        <div class="field">
+            <label class="label">Nome</label>
+            <div class="control">
+                <input class="input" type="text" placeholder="Digite seu nome">
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <div class="field">
+            <label class="label">Email</label>
+            <div class="control">
+                <input class="input" type="email" placeholder="Digite seu email">
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <div class="field">
+            <label class="label">Assunto</label>
+            <div class="control">
+                <input class="input" type="text" placeholder="Digite seu assunto">
+            </div>
+        </div>
+    </div>
+
+
+    <div class="field">
+        <label class="label">Mensagem</label>
+        <div class="control">
+            <textarea class="textarea" name="message" placeholder="Digite aqui sua mensagem"></textarea>
+        </div>
+    </div>
+
+    <div class="field is-grouped">
+        <div class="control">
+            <button class="button is-link">Enviar</button>
+        </div>
+    </div>
+</form>

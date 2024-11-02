3c147a899f3dff7e193e70ac5b97ebae68abad41
Gabriel Binotti <gabrielbinotti.dev@gmail.com>
2024-11-01 20:09:21 -0500

    flash message

--- LoadingInternalPage/app/functions/validate.php
+++ LoadingInternalPage/app/functions/validate.php
@@ -37,3 +37,36 @@ function request()
 
     return $_GET;
 }
+
+
+function flash($key, $message, $type = 'danger')
+{
+    if(!isset($_SESSION['flash'][$key])){
+        $_SESSION['flash'][$key] = "<span class='alert alert-{$type}'>{$message}</span>";
+    }
+}
+
+
+function get($key){
+    if(isset($_SESSION['flash'][$key])){
+        $message = $_SESSION['flash'][$key];
+        unset($_SESSION['flash'][$key]);
+
+        return $message ?? '';
+    }
+}
+
+function isEmpty(){
+
+    $request = request();
+
+    $empty = false;
+
+    foreach ($$request as $key => $value) {
+        if(empty($request[$key])){
+            $empty = true;
+        }
+    }
+
+    return $empty;
+}
--- LoadingInternalPage/server/contato.php
+++ LoadingInternalPage/server/contato.php
@@ -3,6 +3,13 @@
 
 require_once "../app/functions/validate.php";
 
+if(isEmpty()){
+    flash('message','Preencher todos os campos');
+
+    header('Location: mandar para algum lugar');
+}
+
+
 $validate = validate([
     "name"      => 's',
     "email"     => 'e',
